05fce866adfdc17b7427470937428920f85452c7
jacolew <jacolew@gmail.com>
2022-08-30 23:51:54 -0300

    Push Cambios JacquesLew a Categoria Empleado Producto

--- CARRITO-D/CARRITO-D/Models/Categoria.cs
+++ CARRITO-D/CARRITO-D/Models/Categoria.cs
@@ -2,5 +2,11 @@
 {
     public class Categoria
     {
+
+        public string Nombre { get; set; };
+        public string Descripcion { get; set; };
+        public List<StockItem> Productos { get; set; };
+
+
     }
 }
--- CARRITO-D/CARRITO-D/Models/Producto.cs
+++ CARRITO-D/CARRITO-D/Models/Producto.cs
@@ -2,5 +2,13 @@
 {
     public class Producto
     {
+
+        public string Nombre { get; set; };
+        public string Descripcion { get; set; };
+        public int PrecioVigente { get; set; };
+        public boolean Activo { get; set; };
+        public string Categoria { get; set; };
+            
+
     }
 }
