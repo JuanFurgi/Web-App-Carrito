3b9e3d09bf800ca7887fb56695e80cd6a6728d55
jacolew <jacolew@gmail.com>
2022-09-21 01:30:36 -0300

    ErrorMsg Entidad creada, utilizacion de los errores creados en Persona

--- CARRITO-D/CARRITO-D/Models/Persona.cs
+++ CARRITO-D/CARRITO-D/Models/Persona.cs
@@ -1,13 +1,30 @@
-﻿namespace CARRITO_D.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CARRITO_D.Models
 {
     public class Persona
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.MsgReq)]
+        [MaxLength(35, ErrorMessage = ErrorMsg.MsgMaxStr)]
+        [MinLength(2, ErrorMessage = ErrorMsg.MsgMinStr)]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.MsgReq)]
+        [MaxLength(35, ErrorMessage = ErrorMsg.MsgMaxStr)]
         public string UserName { get; set; }
-        public string Apellido { get; set; }    
+
+        [Required(ErrorMessage = ErrorMsg.MsgReq)]
+        [MaxLength(25)]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.MsgReq)]
+        [EmailAddress(ErrorMessage = ErrorMsg.TipoInvalido)]
         public string Email { get; set; }
         public string Direccion { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime FechaAlta { get; set;}
         public int Telefono { get; set; } 
 
