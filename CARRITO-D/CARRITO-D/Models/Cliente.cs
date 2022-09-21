6850de300e26ccc823b6ba720e2bbf08f81153a5
JuanFurgi <juanpifurgi1@gmail.com>
2022-09-21 15:51:45 -0300

    fixes de detalles de codigo

--- CARRITO-D/CARRITO-D/Models/Cliente.cs
+++ CARRITO-D/CARRITO-D/Models/Cliente.cs
@@ -1,8 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace CARRITO_D.Models
 {
     public class Cliente : Persona
-    { 
+    {
+
+//NO SE SI ESTA BIEN PONER LA RegExpression ASI
+        [Required]
+        [RegularExpression(@"[0-9]{8}", ErrorMessage = ErrorMsg.MsgMinStr)]
         public int DNI { get; set; }
         public List<Carrito> Carritos { get; set; }
         public List<Compra> Compras { get; set; }   //Historial de compras Tal vez   
