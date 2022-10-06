b530191899b27dfe9dddad2f6ffae39b6fa9d370
JuanFurgi <juanpifurgi1@gmail.com>
2022-10-06 02:51:16 -0300

    Cambios: Se Agregan controladores de casi todas las entidades Se Crea logo y pone en la pagina

--- CARRITO-D/CARRITO-D/Controllers/HomeController.cs
+++ CARRITO-D/CARRITO-D/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace CARRITO_D.Controllers
 
 
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
